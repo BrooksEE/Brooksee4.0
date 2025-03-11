e0e76771d295086f85243750c6a2c3792a301316
Tanner Olsen <tannerolsen7@gmail.com>
2025-03-11 10:49:23 -0600

    Remove TODO's from main page for now

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,14 +13,11 @@
 <template>
     <TheHeader />
     <TheSideBar />
-    <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+    <!-- <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         <p style="margin: 0; padding: 0;">
-            Next steps off the top of my head:
-            <ul style="padding-left: 30px;">
-                <li>Get the rest of the select options to show for the selected entity/host/event</li>
-            </ul>
+            Ready for more feedb
         </p>
-    </div>
+    </div> -->
 </template>
 <style scoped>
 
